1a9dd58cb88d68929023356fad37f249b28b3f72
crevelo <carlosrevla@hotmail.com>
2021-01-29 14:08:04 -0500

    reparadp error prediccion

--- app.py
+++ app.py
@@ -14,48 +14,36 @@ from sklearn import datasets
 app = Flask(__name__)
 CORS(app)
 
+with open('nutrients_csvfile.csv', newline='') as File:  
+    reader = pd.read_csv(File)
+data_set = pd.DataFrame(reader)
+data_set = reader.drop(["Food"], axis=1)
+data_set = data_set.drop(["Category"], axis=1)
+data_set = data_set.drop(['Measure'], axis=1)
+data_set = data_set.replace("t", 0.2)
+data_set = data_set.replace("t'", 0.2)
+data_set = data_set.replace("a", 0)
+data_set = data_set.astype(str)
+
+for i in data_set:
+    cont=0
+    for j in data_set[i]:
+        j = re.sub(",", "", j)
+        j = re.sub("-","", j)
+        j = re.sub("nan","0", j)
+        data_set[i].values[cont] = j
+        data_set[i].values[cont] = float(data_set[i].values[cont])
+   
+        cont+=1
+print(data_set)
+
+kmeanModel = KMeans(n_clusters=3)
+kmeanModel.fit(data_set)
+
 @app.route('/', methods=['POST'])
 def index():
 
-    with open('nutrients_csvfile.csv', newline='') as File:  
-        reader = pd.read_csv(File)
-    data_set = pd.DataFrame(reader)
-    data_set = reader.drop(["Food"], axis=1)
-    data_set = data_set.drop(["Category"], axis=1)
-    data_set = data_set.drop(['Measure'], axis=1)
-    data_set = data_set.replace("t", 0.2)
-    data_set = data_set.replace("t'", 0.2)
-    data_set = data_set.replace("a", 0)
-    data_set = data_set.astype(str)
-
-    for i in data_set:
-        cont=0
-        for j in data_set[i]:
-            j = re.sub(",", "", j)
-            j = re.sub("-","", j)
-            j = re.sub("nan","0", j)
-            data_set[i].values[cont] = j
-            data_set[i].values[cont] = float(data_set[i].values[cont])
-    
-            cont+=1
-    print(data_set)
-
-    distorcion = []
-
-    n = range(1,10)
-
-    for k in n:
-        kmeanModel = KMeans(n_clusters=k)
-        kmeanModel.fit(data_set)
-        distorcion.append(kmeanModel.inertia_)
-    
-    kmeanModel = KMeans(n_clusters=3)
-
-    kmeanModel.fit(data_set)
-
-    predi = kmeanModel.predict(data_set)
-
-    
+    #predi = kmeanModel.predict(data_set)
 
     produ = {
         "Grams": [request.json['Grams']],
@@ -68,7 +56,7 @@ def index():
     }
     
     product = pd.DataFrame(produ)
-
+    print(product)
     prediccion = int(kmeanModel.predict(product))
 
     print(prediccion)
